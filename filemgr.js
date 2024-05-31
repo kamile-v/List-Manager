2149bc9e80483f51164397a9869a3b4c632845a5
Minnie Cao <144734148+MinnieMwooh@users.noreply.github.com>
2024-05-31 21:45:06 +0000

    update WriteData()

--- filemgr.js
+++ filemgr.js
@@ -10,18 +10,23 @@ async function ReadData() {
     // Read the file
     const data = await fs.readFile(jsonFile, 'utf-8');
     // convert the buffer to a json object and return it
+    console.log('JSON file read successfully.');
     return JSON.parse(data);
   } catch (error) {
     console.error('ReadData() error');
+    return null;
   }
 }
 
 async function WriteData(dataOut) {
   try {
     // Write the file
+    const data = JSON.stringify(dataOut);
+    await fs.writeFile(jsonFile, data, 'utf-8');
 
+    console.log('JSON file written successfully.');
   } catch (error) {
-
+    console.error('WriteData() error');
   }
 }
 
