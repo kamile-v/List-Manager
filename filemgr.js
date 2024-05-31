28c0008d047dccf6e7ccb43aebe6ce74a05a7d15
Minnie Cao <144734148+MinnieMwooh@users.noreply.github.com>
2024-05-31 21:36:45 +0000

    update ReadData()

--- filemgr.js
+++ filemgr.js
@@ -1,12 +1,18 @@
 const fs = require("fs/promises")
+const path = require("path")
+
+const jsonFile = path.join(__dirname, 'Node', 'listdata.json');
 
 async function ReadData() {
   try {
     // Make sure the file exists
+    await fs.access(jsonFile);
     // Read the file
+    const data = await fs.readFile(jsonFile, 'utf-8');
     // convert the buffer to a json object and return it
+    return JSON.parse(data);
   } catch (error) {
-
+    console.error('ReadData() error');
   }
 }
 
