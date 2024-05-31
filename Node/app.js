c0a6d4341e93d532cf68860aee67b77cd48f4ffa
kamile-v <157083698+kamile-v@users.noreply.github.com>
2024-05-31 13:33:49 -0700

    Update app.js

--- Node/app.js
+++ Node/app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 // Define HTTP routes listenting for requests
 app.get("/api", async (req,res) => {
-
+  res.setEncoding("List retrieved");
 })
 
 app.post("/api", async (req,res) => {
